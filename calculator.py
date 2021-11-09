1df5a13f600155214fcb8d985a8dd549b084325d
Pmraut15 <93643442+Pmraut15@users.noreply.github.com>
2021-11-09 10:26:46 -0800

    Add files via upload
    I have uploaded my code source file as fallow!

---
+++ calculator.py
@@ -0,0 +1,24 @@
+a,b=3,9
+#addition of two numbers
+add=a+b
+#subtraction of two numbers
+sub=a-b
+#mutiplication of two numbers
+mul=a*b
+#divion(float) of two numbers
+div1=a/b
+#divion(floor) of two numbers
+div2=a//b
+#power of two numbers
+power=a**b
+#modulo of two numbers
+mod=a%b
+
+# print result
+print(add)
+print(sub)
+print(mul)
+print(div1)
+print(div2)
+print(power)
+print(mod)
